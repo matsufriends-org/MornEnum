084867cc14282bf6d2f6567892f5b67d28318ec4
matsufriends <pikapikan0526@gmail.com>
2025-04-21 01:06:27 +0900

    ==オペレーターの実装

--- MornEnumBase.cs
+++ MornEnumBase.cs
@@ -18,5 +18,17 @@ namespace MornEnum
             set => _key = Values[value];
         }
         protected abstract string[] Values { get; }
+        
+        public static bool operator ==(MornEnumBase a, MornEnumBase b)
+        {
+            if (a is null && b is null) return true;
+            if (a is null || b is null) return false;
+            return a._key == b._key;
+        }
+
+        public static bool operator !=(MornEnumBase a, MornEnumBase b)
+        {
+            return !(a == b);
+        }
     }
 }
